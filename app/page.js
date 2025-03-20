1dfdd085246c78a8b389e3b7ca3c06c82a9a2cd5
shubhampatill00 <spatil@woyce.io>
2025-03-20 13:07:34 +0530

    img

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@
 "use client"
 
 import { useEffect, useState } from "react";
-import About from "./components/about";
+import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Work from "./components/Work";
